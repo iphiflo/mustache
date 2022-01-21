95d91d3f673f462166594fb8626261e2fed85193
iphiflo <87710883+iphiflo@users.noreply.github.com>
2022-01-21 17:30:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function tak_snapshot()
+{
+    save("mustacheFilter.png")
+}
